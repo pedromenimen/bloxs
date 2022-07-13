0a555b0c93b4896423226b0e0b22f5c8cec605fd
Pedro <pedromenimen@gmail.com>
2022-07-12 21:26:48 -0300

    fix: BASE_URL added to .env

--- app/controllers/documentation_controllers/get_documentation.py
+++ app/controllers/documentation_controllers/get_documentation.py
@@ -1,5 +1,7 @@
+import os
 from flask import render_template
 
 
 def get_documentation():
-    return render_template("rapidoc.html")
+    url = os.getenv("BASE_URL")+"/api/rapidoc-json/"
+    return render_template("rapidoc.html", url=url)
