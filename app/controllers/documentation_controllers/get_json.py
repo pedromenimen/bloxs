1f8f4ebef390cd002bbd6ee0afbf089b42d48421
Pedro <pedromenimen@gmail.com>
2022-07-12 21:36:36 -0300

    another test

--- app/controllers/documentation_controllers/get_json.py
+++ app/controllers/documentation_controllers/get_json.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def get_json():
 
-    json_file = os.path.abspath(__file__).split("app")[0] + "app/static/rapidoc.json"
+    json_file = os.path.abspath(__file__).split("app")[0]
     print('\n\n\n\n',json_file,'\n\n\n\n')
     with open(json_file, "r") as file:
         data = json.load(file)
